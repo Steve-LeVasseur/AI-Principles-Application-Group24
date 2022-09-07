f190b3494070aef36fde982b4c84e7c3abd803a0
Steve-LeVasseur <49535801+Steve-LeVasseur@users.noreply.github.com>
2022-09-07 12:42:40 -0400

    Update bubblesort.py
    update

--- bubblesort.py
+++ bubblesort.py
@@ -2,29 +2,24 @@
 
 # DO NOT SUMBIT THIS CODE
 
-def bubbleSort(arr):
-	n = len(arr)
-	# optimize code, so if the array is already sorted, it doesn't need
-	# to go through the entire process
-	swapped = False
-	# Traverse through all array elements
-	for i in range(n-1):
-		# range(n) also work but outer loop will
-		# repeat one time more than needed.
-		# Last i elements are already in place
-		for j in range(0, n-i-1):
+def bubbleSort(L):
+    for i in range(len(L)-1):
+        for j in range(len(L)-i-1):
+            if L[j+1] < L[j]:
+                swap = True
+                L[j], L[j+1] = L[j+1], L[j]
 
-			# traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
-			if arr[j] > arr[j + 1]:
-				swapped = True
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+# def bubbleSort(arr):
+# 	n = len(arr)
+# 	swapped = False
+# 	for i in range(n-1):
+# 		for j in range(0, n-i-1):
+# 			if arr[j] > arr[j + 1]:
+# 				swapped = True
+# 				arr[j], arr[j + 1] = arr[j + 1], arr[j]
 		
-		if not swapped:
-			# if we haven't needed to make a single swap, we
-			# can just exit the main loop.
-			return
+# 		if not swapped:
+# 			return
 
 
 # Driver code to test above
